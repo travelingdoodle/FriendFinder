926996899ba374935b186301a8b9be3cc94bb68c
travelingdoodle <reggiebrown@gmail.com>
2017-09-30 10:28:33 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,109 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Rick",
+      photo: "https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/vdc49xfybuuua6fd2jjx/everything-we-know-so-far-about-rick-and-morty-season-3.jpg",
+      scores: [
+        "5",
+        "3",
+        "5",
+        "1",
+        "1",
+        "5",
+        "5",
+        "5",
+        "1",
+        "1"
+      ]
+    },
+    {
+      name: "Morty",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYDKtKFDJzgcDA70gTCZ5eV3V9lhX_Mc7GfmiscI8_lEL_ebHf",
+      scores: [
+        "3",
+        "5",
+        "2",
+        "1",
+        "1",
+        "1",
+        "4",
+        "3",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "Hitler",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      scores: [
+        "5",
+        "3",
+        "2",
+        "2",
+        "1",
+        "5",
+        "5",
+        "4",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "Chewy",
+      photo: "https://pbs.twimg.com/profile_images/616737241340080128/0wVsbb_P_400x400.jpg",
+      scores: [
+        "3",
+        "5",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "4",
+        "3",
+        "5"
+      ]
+    },
+    {
+      name: "Trump",
+      photo: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/05/12/104466932-PE_Color.240x240.jpg?v=1494613853",
+      scores: [
+        "5",
+        "5",
+        "2",
+        "1",
+        "1",
+        "5",
+        "1",
+        "1",
+        "1",
+        "5"
+      ]
+    },
+    {
+      name: "Burger Chef",
+      photo: "http://www.freewebs.com/burgerchef/1973%20logo.JPG",
+      scores: [
+        "5",
+        "5",
+        "1",
+        "1",
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
